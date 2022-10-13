946eb1e7466842051165c574d35a034e60a8f152
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-10-13 22:41:30 +0900

    1 - 6 연산자(1)

---
+++ operator.js
@@ -0,0 +1,32 @@
+// const shoesPrice = 200000
+// console.log('이 신발의 가격은' + shoesPrice + '원 입니다.')
+// console.log(`이 신발의 가격은 ${shoesPrice}원 입니다.`)
+
+
+
+// console.log(2 + 1)
+// console.log(2 - 1)
+// console.log(2 * 3)
+// console.log(4 / 2)
+// console.log(10 % 3)
+// console.log(10 ** 2)
+
+
+
+// let count = 1
+// const preCount = ++count
+// console.log(`count: ${count}, preCount: ${preCount}`)
+
+// let count = 1
+// const preCount = count++
+// console.log(`count: ${count}, preCount: ${preCount}`)
+
+const shirtsPrice = 100000
+const shoesPrice = 80000
+let totalPrice = 0
+totalPrice += shirtsPrice
+console.log(totalPrice)
+totalPrice += shoesPrice
+console.log(totalPrice)
+totalPrice -= shirtsPrice
+console.log(totalPrice)
