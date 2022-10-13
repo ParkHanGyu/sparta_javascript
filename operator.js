df2aa4df247e7ab485ca5a6f4f97913966026906
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-10-13 23:45:54 +0900

    1-7 연산자(2)

--- operator.js
+++ operator.js
@@ -2,7 +2,7 @@
 // console.log('이 신발의 가격은' + shoesPrice + '원 입니다.')
 // console.log(`이 신발의 가격은 ${shoesPrice}원 입니다.`)
 
-
+///////////////////////////////////////////////////////////////////////////////////////////
 
 // console.log(2 + 1)
 // console.log(2 - 1)
@@ -11,7 +11,7 @@
 // console.log(10 % 3)
 // console.log(10 ** 2)
 
-
+///////////////////////////////////////////////////////////////////////////////////////////
 
 // let count = 1
 // const preCount = ++count
@@ -21,12 +21,41 @@
 // const preCount = count++
 // console.log(`count: ${count}, preCount: ${preCount}`)
 
-const shirtsPrice = 100000
-const shoesPrice = 80000
-let totalPrice = 0
-totalPrice += shirtsPrice
-console.log(totalPrice)
-totalPrice += shoesPrice
-console.log(totalPrice)
-totalPrice -= shirtsPrice
-console.log(totalPrice)
+// const shirtsPrice = 100000
+// const shoesPrice = 80000
+// let totalPrice = 0
+// totalPrice += shirtsPrice
+// console.log(totalPrice)
+// totalPrice += shoesPrice
+// console.log(totalPrice)
+// totalPrice -= shirtsPrice
+// console.log(totalPrice)
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// console.log(1 < 2)
+// console.log(1 <= 2)
+// console.log(1 > 2)
+// console.log(1 >= 2)
+
+// let isOnSale = true
+// let isDiscountItem = true
+// console.log(isOnSale && isDiscountItem) // true && true 이므로 true
+// console.log(isOnSale || isDiscountItem) // true || true 이므로 true
+// isOnSale = false
+// console.log(isOnSale && isDiscountItem) // false && true 이므로 false
+// console.log(isOnSale || isDiscountItem) // false || true 이므로 true
+// isDiscountItem = false
+// console.log(isOnSale && isDiscountItem) // false && false 이므로 false
+// console.log(isOnSale || isDiscountItem) // false || false 이므로 false
+
+// console.log(!isOnSale) // !false 이므로 true
+// console.log(1 === 1) // true
+// console.log(1 === 2) // false
+// console.log('Javascript' === 'Javascript') // true
+// console.log('Javascript' === 'javascript') // 대소문자나 띄워쓰기도 다 정확히 일치해야 해요. 따라서 false
+
+const a = 200000
+const b = 100000
+let total = a + b
+console.log(`총 ${total * 0.8} 원에 물건을 구입합니다.`)
