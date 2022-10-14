233296c6a769c9d0704693c63f5a6a73082f3024
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-10-14 21:31:01 +0900

    1-9 조건문(2)

---
+++ if_2.js
@@ -0,0 +1,19 @@
+// const shoesPrice = 30000
+// if (shoesPrice < 40000) {
+//     console.log('이 신발을 사겠습니다.')
+// }   else if (shoesPrice <= 50000) {
+//         console.log('고민을 해볼게요')
+// }   else {
+//         console.log('너무 비싸요!')
+// }
+
+//////////////////////////////////////////////////////////////
+
+const a = 6
+if (a < 2) {
+    console.log('걸어가자')
+} else if (a >= 2 && a < 5) {
+    console.log('택시를 타자')
+} else {
+    console.log('기차를 타자')
+}
