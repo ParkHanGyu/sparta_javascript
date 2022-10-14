4dd67a66727ad31eabc2e6c87f220baa3b518d7b
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-10-15 00:09:02 +0900

    1-11반복문(2) for문 내용

--- loop.js
+++ loop.js
@@ -1,6 +1,9 @@
-let temperature = 20
+// let temperature = 20
+// while (a < 25) {
+//     console.log(`${temperature}도 정도면 적당한 온도입니다.`)
+//     temperature++
+// }
 
-while (a < 25) {
+for (let temperature = 20; temperature < 25; temperature++) {
     console.log(`${temperature}도 정도면 적당한 온도입니다.`)
-    temperature++
 }
