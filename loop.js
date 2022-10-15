593960a8562349ffcd0b74ab0a0b19ff7aa19d69
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2022-10-15 23:24:54 +0900

    반복문, 조건문으로 홀수, 짝수 구하기

--- loop.js
+++ loop.js
@@ -3,7 +3,21 @@
 //     console.log(`${temperature}도 정도면 적당한 온도입니다.`)
 //     temperature++
 // }
+// for (let temperature = 20; temperature < 25; temperature++) {
+//     console.log(`${temperature}도 정도면 적당한 온도입니다.`)
+// }
 
-for (let temperature = 20; temperature < 25; temperature++) {
-    console.log(`${temperature}도 정도면 적당한 온도입니다.`)
+
+// for (let number = 1; number < 11; number++) {
+//     if (number % 3 === 0) {
+//         console.log(`${number}은(은) 3으로 나누었을때 떨어지는 숫자입니다.`)
+//     }
+// }
+
+
+for (let number = 1; number < 21; number++) {
+    if (number % 2 === 0) {
+        console.log(`${number}은 짝수입니다.`)
+    } else
+        console.log(`${number}은 홀수입니다.`)
 }
